a7a5b16d41bce1637f6ea345354b67535fbf625a
Éverton Assis <eassis.vieira@gmail.com>
2022-07-12 08:18:24 -0300

    Add missing plugins to plugin list

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -4,12 +4,14 @@ local plugins = {
     'nvim-lsp-installer',
     'nvim-cmp',
     'nvim-tree',
-    'spellsitter'
+    'spellsitter',
+    'telescope',
+    'nvim-treesitter'
 }
 
 for _, plugin in ipairs(plugins) do
     local ok, err = pcall(require, 'plugins.' .. plugin)
-    if not ok then 
+    if not ok then
         vim.api.nvim_err_writeln('Failed to load module ' .. plugin .. '\n' .. err)
     end
 end
