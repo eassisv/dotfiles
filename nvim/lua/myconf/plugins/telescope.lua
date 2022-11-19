fbd876c5e73bdc7fb12a944e9c2f6efea103de0c
Éverton Assis <eassis.vieira@gmail.com>
2022-11-19 08:33:00 -0300

    Change lsp_references to use telescope

--- nvim/lua/myconf/plugins/telescope.lua
+++ nvim/lua/myconf/plugins/telescope.lua
@@ -16,3 +16,4 @@ local nmap = require("myconf.mappings").nmap
 nmap("<leader>ff", "<cmd>Telescope find_files<cr>")
 nmap("<leader>fg", "<cmd>Telescope live_grep<cr>")
 nmap("\\\\", "<cmd>Telescope buffers<cr>")
+nmap("<leader>lr", "<Cmd>Telescope lsp_references<cr>")
