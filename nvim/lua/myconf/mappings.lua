7a482dd7a928a2455e3d28bffa7d6aa1f0865307
Éverton Assis <eassis.vieira@gmail.com>
2023-01-19 01:13:32 -0300

    fixing and improving

--- nvim/lua/myconf/mappings.lua
+++ nvim/lua/myconf/mappings.lua
@@ -1,7 +1,9 @@
 local map = vim.keymap.set
 
+-- TODO: merge options
 local function _map(mode)
   return function(lhs, rhs, opts)
+    opts = vim.tbl_extend("force", { silent = true, remap = false }, opts or {})
     map(mode, lhs, rhs, opts)
   end
 end
@@ -9,19 +11,19 @@ end
 local nmap = _map("n")
 local vmap = _map("v")
 
-nmap("<leader>w", ":w!<cr>")
-nmap("<leader>l", ":nohl<cr>")
-nmap(",,s", ":so %<cr>", { silent = false })
+nmap("<leader>w", ":w!<cr>", { desc = "Quick save" })
+nmap("<leader>l", ":nohl<cr>", { desc = "No highlight!" })
+nmap(",,s", ":so %<cr>", { silent = false, desc = "Reload config file" })
 
 -- windows change
-nmap("<C-j>", "<C-W>j")
-nmap("<C-k>", "<C-W>k")
-nmap("<C-h>", "<C-W>h")
-nmap("<C-l>", "<C-W>l")
+nmap("<C-j>", "<C-W>j", { desc = "Move to window below" })
+nmap("<C-k>", "<C-W>k", { desc = "Move to window above" })
+nmap("<C-h>", "<C-W>h", { desc = "Move to left window" })
+nmap("<C-l>", "<C-W>l", { desc = "Move to right window" })
 
 -- move lines
-nmap("<M-j>", "mz:m+<cr>`z")
-nmap("<M-k>", "mz:m-2<cr>`z")
+nmap("<M-j>", "mz:m+<cr>`z", { desc = "Move line up" })
+nmap("<M-k>", "mz:m-2<cr>`z", { desc = "Move line down" })
 
 nmap("<C-d>", "<C-d>zz")
 nmap("<C-u>", "<C-u>zz")
@@ -30,8 +32,8 @@ nmap("<leader>te", ":Explore<cr>")
 nmap("<leader>gs", ":Git<cr>")
 
 -- Move lines and selections
-vmap("<M-j>", ":m'>+<cr>`<my`>mzgv`yo`z")
-vmap("<M-k>", ":m'<-2<cr>`<my`>mzgv`yo`z")
+vmap("<M-j>", ":m'>+<cr>`<my`>mzgv`yo`z", { desc = "Move selection up" })
+vmap("<M-k>", ":m'<-2<cr>`<my`>mzgv`yo`z", { desc = "Move selection down" })
 
 vim.cmd("tnoremap <C-[> <C-\\><C-n>")
 
